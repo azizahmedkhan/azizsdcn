22b6fef6de072cf24445068c46dc104dac19db1a
AzizAhmedKhan <azizahmed.khan@gmail.com>
2017-03-20 18:24:19 +1300

    Adding MnistCNN in git. Leading data

--- TrafficSignClassifier/mnist/MnistCNN.py
+++ TrafficSignClassifier/mnist/MnistCNN.py
@@ -8,3 +8,18 @@ learning_rate = 0.00001
 epochs = 10
 batch_size = 128
 
+#Number of samples to calculate calidation abd accuracy
+# Decrease this if running ouit of memeory to calculate accuracy
+test_validation_size = 256
+
+#Network Parameters
+n_classes = 10 #Mnist classes (0-9 Digits)
+dropout = 0.75 # Dropout, probability to keep units
+
+#Store layers weight and bias
+weights = {
+    'wc1' : tf.Variable(tf.random_normal([5,5,1,32])),
+    'wc2' : tf.Varialbe(tf.random_normal([5,5,32,64])),
+    'wd1' : tf.Variable(tf.random_normal([7*7*64, 1024])),
+    'out' : tf.Variable(tf.random_normal([1024, n_classes]))
+}
